77e22639dcae36860509b751f987b38118cd9416
ChainsAre2Tight <124983704+ChainsAre2Tight@users.noreply.github.com>
2024-04-10 12:11:01 +0300

    adds message sending and some user rights handling

--- application/feed/handlers.py
+++ application/feed/handlers.py
@@ -1,23 +1,25 @@
 from flask import jsonify, request, make_response, Response
-from utils.wrappers import require_access_token, handle_user_existance
-import utils.my_dataclasses as dataclass
-import sqlalchemy.exc as sql
 
+from utils.wrappers import require_access_token, handle_user_rights
+import utils.my_dataclasses as dataclass
 from feed.app import app
 import feed.queries as q
 
 @app.route('/api/data', methods=['GET'])
 @require_access_token
-def ping(token) -> tuple[Response, int]:
+def ping(_) -> tuple[Response, int]:
     """This endpoint provides a way to check service avaliability"""
 
     return jsonify('pinged'), 200
 
 @app.route('/api/data/chats', methods=['GET'])
 @require_access_token
-@handle_user_existance
+@handle_user_rights
 def get_chats(token: dataclass.Token) -> tuple[Response, int]:
-    """This endpoint provides a way to get all chats that are accessible to a requesting user"""
+    """
+    This endpoint provides a way to get all chats\
+    that are accessible to a requesting user
+    """
     
     chats = dataclass.convert_dataclass_to_dict(
         q.get_chats_by_sessionId(token.sessionId)
@@ -26,9 +28,12 @@ def get_chats(token: dataclass.Token) -> tuple[Response, int]:
 
 @app.route('/api/data/users', methods=['GET'])
 @require_access_token
-@handle_user_existance
+@handle_user_rights
 def get_users(token: dataclass.Token) -> tuple[Response, int]:
-    """This endpoint provides a way to get info of users sharing a chat with the issuer"""
+    """
+    This endpoint provides a way to get info of users\
+    sharing a chat with the issuer
+    """
     
     users = dataclass.convert_dataclass_to_dict(
         q.get_users_by_sessionId(token.sessionId)
@@ -37,11 +42,42 @@ def get_users(token: dataclass.Token) -> tuple[Response, int]:
 
 @app.route('/api/data/chats/<int:id>/messages', methods=['GET'])
 @require_access_token
-@handle_user_existance
+@handle_user_rights
 def get_messages_by_chat(token: dataclass.Token, id: int) -> tuple[Response, int]:
-    """This endpoint provides a way to get all mesages within a specified chat if a user has access to it"""
+    """
+    This endpoint provides a way to get all mesages \
+    within a specified chat if a user has access to it
+    """
 
     messages = dataclass.convert_dataclass_to_dict(
         q.get_messages_by_chat_id(chat_id=id, sessionId=token.sessionId)
     )
     return jsonify(messages), 200
+
+@app.route('/api/data/chats/<int:id>/messages', methods=['POST'])
+@require_access_token
+@handle_user_rights
+def send_message(token: dataclass.Token, id: int) -> tuple[Response, int]:
+    """
+    This endpoint serves to provide a way to send new messages\
+        to database and notify all clients that can access it
+    """
+    data = request.get_json()
+    # construct dataclass object
+    raw_message = dataclass.Message(
+        id=None,
+        body=data['body'],
+        timestamp=data['timestamp'],
+        chat_id=id,
+        author_id=None
+    )
+    # send to database and fill missing message data
+    processesed_message = q.store_message(
+        sessionId=token.sessionId,
+        message=raw_message
+    )
+    # if successful, send message data to notification server
+    pass # TODO send data to notification server
+
+    # return success to user
+    return jsonify(processesed_message.__dict__), 201
--- application/feed/queries.py
+++ application/feed/queries.py
@@ -1,8 +1,10 @@
 import database.models as models
 from feed.app import app, db
 import utils.my_dataclasses as dataclass
+import sqlalchemy.exc
+import utils.my_exceptions as exc
 
-def _get_user_by_sessionId(sessionId: str) -> models.Session:
+def _get_user_by_sessionId(sessionId: str) -> models.User:
     return db.session.query(models.Session).\
         filter(models.Session.uuid == sessionId).one().user
 
@@ -20,10 +22,17 @@ def _get_users_by_chats(chats: list[models.Chat]) -> list[models.User]:
     return users
 
 def _get_messages_by_chat_id_and_user(chat_id: int, user: models.User) -> list[models.Message]:
-    # TODO if user is not in chat raise an exception
+    try:
+        chat = db.session.query(models.Chat)\
+            .filter(
+                models.Chat.id == chat_id,
+                models.Chat.users.any(models.User.id == user.id)
+            ).one()
+    except sqlalchemy.exc.NoResultFound:
+        raise exc.NoAcccessException(f'{user.username} Attempted to access chat №{chat_id}')
+
     messages = db.session.query(models.Message).filter(
-        models.Message.chat_id == chat_id,
-        models.Message.chat.has(models.Chat.users.any(models.User.id == user.id))
+        models.Message.chat_id == chat.id
     ).all()
     return messages
 
@@ -54,3 +63,21 @@ def get_messages_by_chat_id(chat_id: int, sessionId: str) -> list[dataclass.Mess
         ),
         dataclass.Message
     )
+
+def store_message(sessionId: str, \
+                  message: dataclass.Message) -> dataclass.Message:
+    
+    # get user info from database
+    user = _get_user_by_sessionId(sessionId=sessionId)
+    message.author_id = user.id
+
+    # send messsage to database
+    model_message: models.Message = message.to_model(model=models.Message)
+    db.session.add(model_message)
+    db.session.commit()
+    
+    # update dataclass info
+    message.id = model_message.id
+
+    return message
+    
