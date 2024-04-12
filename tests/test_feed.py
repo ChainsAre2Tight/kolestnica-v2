4efbf079b677f3912f8eb7d2e543cc208d109121
ChainsAre2Tight <124983704+ChainsAre2Tight@users.noreply.github.com>
2024-04-12 17:37:12 +0300

    separates status code check from payload check

--- tests/test_feed.py
+++ tests/test_feed.py
@@ -47,10 +47,11 @@ class TestChats(unittest.TestCase):
             url='http://127.0.0.1:5010/api/data/chats',
             headers={'Authorization': token_pointing_to_user_1}
         )
-    
+
+        self.assertEqual(r.status_code, 200)
         self.assertEqual(
-            (r.json(), r.status_code),
-            ([{'id': 1, 'image_href': None, 'message_ids': [1, 2], 'name': 'test chat #1 static', 'user_ids': [1, 2]}], 200)
+            r.json(),
+            [{'id': 1, 'image_href': None, 'message_ids': [1, 2], 'name': 'test chat #1 static', 'user_ids': [1, 2]}]
         )
     
     def test_request_chats_for_null_user(self):
@@ -73,13 +74,14 @@ class TestUsers(unittest.TestCase):
             url='http://127.0.0.1:5010/api/data/users',
             headers={'Authorization': token_pointing_to_user_1}
         )
-    
+
+        self.assertEqual(r.status_code, 200)
         self.assertEqual(
-            (r.json(), r.status_code),
-            ([
+            r.json(),
+            [
                 {'id': 1, 'username': 'test_1', 'alias': 'test user #1', 'image_href': default_href},
                 {'id': 2, 'username': 'test_2', 'alias': 'test user #2', 'image_href': default_href}
-            ], 200)
+            ]
         )
     
     def test_request_chats_for_user_in_chat_2(self):
@@ -88,12 +90,13 @@ class TestUsers(unittest.TestCase):
             url='http://127.0.0.1:5010/api/data/users',
             headers={'Authorization': token_pointing_to_user_4}
         )
-    
+
+        self.assertEqual(r.status_code, 200)
         self.assertEqual(
-            (r.json(), r.status_code),
-            ([
+            r.json(),
+            [
                 {'id': 4, 'username': 'test_4', 'alias': 'test user #4', 'image_href': default_href},
-            ], 200)
+            ]
         )
     
     def test_request_chats_for_user_not_in_chat(self):
@@ -102,11 +105,9 @@ class TestUsers(unittest.TestCase):
             url='http://127.0.0.1:5010/api/data/users',
             headers={'Authorization': token_pointing_to_user_3}
         )
-    
-        self.assertEqual(
-            (r.json(), r.status_code),
-            ([], 200)
-        )
+
+        self.assertEqual(r.status_code, 200)
+        self.assertEqual(r.json(), [])
 
 class TestStaticMessages(unittest.TestCase):
 
@@ -116,13 +117,14 @@ class TestStaticMessages(unittest.TestCase):
             url='http://127.0.0.1:5010/api/data/chats/1/messages',
             headers={'Authorization': token_pointing_to_user_1}
         )
-    
+
+        self.assertEqual(r.status_code, 200)
         self.assertEqual(
-            (r.json(), r.status_code),
-            ([
+            r.json(),
+            [
                 {'author_id': 1,'body': 'hello world!','chat_id': 1,'id': 1,'timestamp': 1000000},
                 {'author_id': 2,'body': 'go away loser','chat_id': 1,'id': 2,'timestamp': 10000000}
-            ], 200)
+            ],
         )
     
     def test_request_messages_for_user_not_in_chat(self):
@@ -145,16 +147,17 @@ class TestDynamicMessage(unittest.TestCase):
             url='http://127.0.0.1:5010/api/data/chats/2/messages',
             headers={'Authorization': token_pointing_to_user_4}
         )
-    
+
+        self.assertEqual(r.status_code, 200)
         self.assertEqual(
-            (r.json(), r.status_code),
-            ([{
+            r.json(),
+            [{
                 'author_id': 4,
                 'body': 'static message',
                 'chat_id': 2,
                 'id': 3,
                 'timestamp': 123321123
-            }], 200)
+            }]
         )
     
     def test_send_valid_message(self):
@@ -168,11 +171,12 @@ class TestDynamicMessage(unittest.TestCase):
             }
         )
 
+        self.assertEqual(r.status_code, 201)
         self.assertEqual(
-            (r.json(), r.status_code),
-            ({
+            r.json(),
+            {
                 'msg_id': 4,
-            }, 201)
+            }
         )
     
     def test_get_sent_message_by_user_in_chat(self):
@@ -181,23 +185,23 @@ class TestDynamicMessage(unittest.TestCase):
             url='http://127.0.0.1:5010/api/data/chats/2/messages',
             headers={'Authorization': token_pointing_to_user_4}
         )
-    
+
+        self.assertEqual(r.status_code, 200)
         self.assertEqual(
-            (r.json(), r.status_code),
-            ([{
+            r.json(),
+            [{
                 'author_id': 4,
                 'body': 'static message',
                 'chat_id': 2,
                 'id': 3,
                 'timestamp': 123321123
-            },
-            {
+            }, {
                 'author_id': 4,
                 'body': 'i sent this message ;)',
                 'chat_id': 2,
                 'id': 4,
                 'timestamp': 1234567890
-            }], 200)
+            }]
         )
     
     def test_get_sent_message_by_user_not_in_chat(self):
@@ -254,10 +258,11 @@ class TestDynamicMessage(unittest.TestCase):
             url='http://127.0.0.1:5010/api/data/chats/2/messages/4',
             headers={'Authorization': token_pointing_to_user_4}
         )
-    
+
+        self.assertEqual(r.status_code, 200)
         self.assertEqual(
-            (r.json(), r.status_code),
-            ({'msg_id': 4}, 200)
+            r.json(),
+            {'msg_id': 4}
         )
 
 class TestChatActions(unittest.TestCase):
@@ -269,10 +274,11 @@ class TestChatActions(unittest.TestCase):
             headers={'Authorization': token_pointing_to_user_4},
             json={'chat_name': 'dynamically created chat'}
         )
-    
+
+        self.assertEqual(r.status_code, 201)
         self.assertEqual(
-            (r.json()['chat_id'], r.status_code),
-            (3, 201)
+            r.json()['chat_id'],
+            3
         )
     
     def test_verify_chat_creation(self):
@@ -282,12 +288,13 @@ class TestChatActions(unittest.TestCase):
             headers={'Authorization': token_pointing_to_user_4}
         )
 
+        self.assertEqual(r.status_code, 200)
         self.assertEqual(
-            (r.json(), r.status_code),
-            ([
+            r.json(),
+            [
                 {'id': 2, 'image_href': None, 'message_ids': [3], 'name': 'test chat #2 dynamic', 'user_ids': [4]},
                 {'id': 3, 'image_href': None, 'message_ids': [], 'name': 'dynamically created chat', 'user_ids': [4]}
-            ], 200)
+            ]
         )
     
     def test_verify_user_list_before(self):
@@ -297,11 +304,12 @@ class TestChatActions(unittest.TestCase):
             headers={'Authorization': token_pointing_to_user_4}
         )
 
+        self.assertEqual(r.status_code, 200)
         self.assertEqual(
-            (r.json(), r.status_code),
-            ([
+            r.json(),
+            [
                 {'id': 4, 'username': 'test_4', 'alias': 'test user #4', 'image_href': default_href}
-            ], 200)
+            ]
         )
     
     def test_add_user_to_chat_user_has_no_access_to(self):
@@ -312,9 +320,10 @@ class TestChatActions(unittest.TestCase):
             json={'username': 'some_user'}
         )
 
+        self.assertEqual(r.status_code, 404)
         self.assertEqual(
-            (r.json()['Error'], r.status_code),
-            ('Cannot access requested data', 404)
+            r.json()['Error'],
+            'Cannot access requested data'
         )
 
     def test_add_user_that_doesnt_exist(self):
@@ -325,9 +334,10 @@ class TestChatActions(unittest.TestCase):
             json={'username': 'some_user'}
         )
 
+        self.assertEqual(r.status_code, 404)
         self.assertEqual(
-            (r.json()['Error'], r.status_code),
-            ('Requested user does not exist', 404)
+            r.json()['Error'],
+            'Requested user does not exist'
         )
     
     def test_add_user_that_is_already_in_chat(self):
@@ -338,9 +348,10 @@ class TestChatActions(unittest.TestCase):
             json={'username': 'test_2'}
         )
 
+        self.assertEqual(r.status_code, 406)
         self.assertEqual(
-            (r.json()['Error'], r.status_code),
-            ('Requirement is already fullfilled', 406)
+            r.json()['Error'],
+            'Requirement is already fullfilled'
         )
     
     def test_add_yourself(self):
@@ -351,9 +362,10 @@ class TestChatActions(unittest.TestCase):
             json={'username': 'test_1'}
         )
 
+        self.assertEqual(r.status_code, 406)
         self.assertEqual(
-            (r.json()['Error'], r.status_code),
-            ('Requirement is already fullfilled', 406)
+            r.json()['Error'],
+            'Requirement is already fullfilled'
         )
     
     def test_add_valid_user(self):
@@ -364,9 +376,10 @@ class TestChatActions(unittest.TestCase):
             json={'username': 'test_1'}
         )
 
+        self.assertEqual(r.status_code, 201)
         self.assertEqual(
-            (r.json(), r.status_code),
-            ({'id': 1, 'username': 'test_1', 'alias': 'test user #1', 'image_href': default_href}, 201)
+            r.json(),
+            {'id': 1, 'username': 'test_1', 'alias': 'test user #1', 'image_href': default_href}
         )
 
     def test_verify_user_list_after(self):
@@ -376,11 +389,10 @@ class TestChatActions(unittest.TestCase):
             headers={'Authorization': token_pointing_to_user_4}
         )
 
-        print(r.json())
-
+        self.assertEqual(r.status_code, 200)
         self.assertEqual(
-            ({i['id'] for i in r.json()}, r.status_code),
-            ({1, 4}, 200)
+            {i['id'] for i in r.json()},
+            {1, 4}
         )
 
 if __name__ == "__main__":
