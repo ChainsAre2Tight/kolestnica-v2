43dc1e2f9dffdd28385c4a16b066281d39453c03
ChainsAre2Tight <124983704+ChainsAre2Tight@users.noreply.github.com>
2024-04-12 17:37:57 +0300

    adds an interface for JWT decoding

--- application/utils/wrappers.py
+++ application/utils/wrappers.py
@@ -6,10 +6,35 @@ import json
 import utils.my_exceptions as exc
 from flask import Response
 import os
+from typing_extensions import Literal
 
 from utils.my_dataclasses import Token
 
-TokenPublicKey = os.environ.get('TOKEN-OPEN-KEY') or 'secret'
+TokenPublicKey = os.environ.get('TOKEN-PUBLIC-KEY') or 'secret'
+
+def decode_token(
+        raw_token: str,
+        algorithm: Literal['HS256', 'RS256'] = 'HS256',
+        verify_expiration: bool = True
+    ) -> Token:
+    """
+    Decodes a token
+
+    :params:
+        str raw_token: string containing JWT token
+        str algorithm: signature algorithm
+        bool verify_expiration: if set to False will skip expiration check
+    
+    :returns dataclass.Token: dataclass Token object containing decoded token data
+    """
+    return Token(
+        **jwt.decode(
+            raw_token,
+            algorithms=[algorithm],
+            options={'verify_exp': verify_expiration},
+            key=TokenPublicKey
+        )
+    )
 
 def require_access_token(
         func: Callable | None=None,
@@ -20,9 +45,9 @@ def require_access_token(
 
     :params verify_exp: optional parameter, if set to True skips token expiration check
     :returns:
-        decorated function if token verification passes
-        HTTP 401 if token signature check failes
-        HTTP 403 if token was expired
+        decorated function: if token verification passes
+        HTTP 401: if token signature check failes
+        HTTP 403: if token was expired
     """
 
     if func is None:
@@ -33,15 +58,11 @@ def require_access_token(
         global TokenPublicKey
         try:
             raw_token: str = dict(request.headers)['Authorization']
-            token: Token = Token(
-                **jwt.decode(
-                        raw_token,
-                        algorithms=['HS256'],
-                        verify=True,
-                        key=TokenPublicKey,
-                        options={'verify_exp': verify_expiry}
-                    )
-                )
+            token: Token = decode_token(
+                raw_token=raw_token,
+                algorithm='HS256',
+                verify_expiration=verify_expiry
+            )
         
         except (jwt.DecodeError, jwt.InvalidSignatureError, KeyError, json.JSONDecodeError):
             return jsonify({
@@ -55,6 +76,47 @@ def require_access_token(
             }), 403
         
         return func(token, *args, **kwargs)
+    
+    return decorated_function
+
+def require_refresh_token(
+        func: Callable | None=None,
+    ) -> tuple[Response, int] | Callable:
+    """
+    Checks for a refresh token in the request cookies and provides \
+        both its raw and decoded data to decorated function
+
+    :returns:
+        decorated function: if token verification passes
+        HTTP 401: if token signature check failes
+        HTTP 403: if token was expired
+    """
+
+    if func is None:
+        return partial(require_access_token)
+
+    @wraps(func)
+    def decorated_function(*args, **kwargs):
+        global TokenPublicKey
+        try:
+            raw_token: str = request.cookies['r']
+            decoded_token: Token = decode_token(
+                raw_token=raw_token,
+                algorithm='HS256'
+            )
+        
+        except (jwt.DecodeError, jwt.InvalidSignatureError, KeyError, json.JSONDecodeError):
+            return jsonify({
+                'Staus': 'Error',
+                'details': 'Refresh token is missing or is invalid'
+            }), 401
+        except jwt.ExpiredSignatureError:
+            return jsonify({
+                'Staus': 'Error',
+                'details': 'Refresh token is expired'
+            }), 403
+        
+        return func(raw_token, decoded_token, *args, **kwargs)
     
     return decorated_function
 
