a31cbef426b8dd449f7498efa36229ecaa444df4
ChainsAre2Tight <124983704+ChainsAre2Tight@users.noreply.github.com>
2024-04-12 18:41:59 +0300

    adds a base parent HTTPException class

--- application/utils/my_exceptions.py
+++ application/utils/my_exceptions.py
@@ -1,32 +1,35 @@
-class UserNotExistsException(BaseException):
+class HTTPException(BaseException):
+    """A base class for exceptions that invoke an HTTP error response"""
+
+class UserNotExistsException(HTTPException):
     """Raised when session making a request is terminated or didnt exist in the first place"""
 
-class NoAcccessException(BaseException):
+class NoAcccessException(HTTPException):
     """Raised when user atempts to request data he has to right to access"""
 
-class NotPermittedException(BaseException):
+class NotPermittedException(HTTPException):
     """Raised when user can access content but cannot perform requested action against it"""
 
-class UserNotFoundException(BaseException):
+class UserNotFoundException(HTTPException):
     """Raised when specified user cannot be found in the database"""
 
-class SessionNotFound(BaseException):
+class SessionNotFound(HTTPException):
     """Raised when session with specified uuid cannot be found in the database"""
 
-class RequestAlreadyFullfilledException(BaseException):
+class RequestAlreadyFullfilledException(HTTPException):
     """Raised when requested action is not needed as requirement is already fullfilled"""
 
-class DuplicateLoginException(BaseException):
+class DuplicateLoginException(HTTPException):
     """Raised when user attempts to register account to an existing login (email)"""
 
-class DuplicateUsernameException(BaseException):
+class DuplicateUsernameException(HTTPException):
     """Raised when user attempts to change his username onto an already taken one"""
 
-class InvalidLoginData(BaseException):
+class InvalidLoginData(HTTPException):
     """Raised when user provides an invalid login/password pair on login"""
 
-class AlreadyLoggedIn(BaseException):
+class AlreadyLoggedIn(HTTPException):
     """Raised when user tries to log in to different account from the same session"""
 
-class DeprecatedRefreshToken(BaseException):
+class DeprecatedRefreshToken(HTTPException):
     """Raised when user tries to refresh token pair with an old or invalid refresh token"""
