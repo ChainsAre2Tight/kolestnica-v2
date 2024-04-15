c3f534f022855e556be1c668dcbd204085ddc18d
ChainsAre2Tight <124983704+ChainsAre2Tight@users.noreply.github.com>
2024-04-15 15:20:06 +0300

    separates dababase url into multiple env variables

--- backend/application/database/server.py
+++ backend/application/database/server.py
@@ -4,6 +4,12 @@ import os
 
 app = Flask('database manager')
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+db_url = os.environ.get('DB_URL')
+db_uasername = os.environ.get('DB_USERNAME')
+db_password = os.environ.get('DB_PASSWORD')
+db_name = os.environ.get('DB_NAME')
+db_port = os.environ.get('DB_PORT')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f'{db_url}://{db_uasername}:{db_password}@db:{db_port}/{db_name}'
 
 db.init_app(app)
--- backend/application/feed/app.py
+++ backend/application/feed/app.py
@@ -4,7 +4,13 @@ import os
 
 app = Flask('Feed')
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+db_url = os.environ.get('DB_URL')
+db_uasername = os.environ.get('DB_USERNAME')
+db_password = os.environ.get('DB_PASSWORD')
+db_name = os.environ.get('DB_NAME')
+db_port = os.environ.get('DB_PORT')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f'{db_url}://{db_uasername}:{db_password}@db:{db_port}/{db_name}'
 app.config['SECRET_KEY'] = os.environ.get('FLASK-SECRET-KEY')
 
 app.config['SQLALCHEMY_ECHO'] = False
