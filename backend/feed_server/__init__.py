6a278e2e78b6c30854edd004bf71c242c4d47b36
ChainsAre2Tight <124983704+ChainsAre2Tight@users.noreply.github.com>
2024-04-18 23:17:25 +0300

    revamps some env variables initialization

--- backend/feed_server/__init__.py
+++ backend/feed_server/__init__.py
@@ -28,9 +28,14 @@ db.init_app(app)
 
 
 # celery
+redis_host = os.environ.get('REDIS_HOST')
+redis_port = os.environ.get('REDIS_PORT')
+redis_queue_db = os.environ.get('REDIS_QUEUE_DB')
+
+redis_url = f'redis://{redis_host}:{redis_port}/{redis_queue_db}'
 celery = Celery(
     'Feed celery',
-    broker=os.environ.get('CELERY_BROKER_URL')
+    broker=redis_url
 )
 
 
--- backend/notification_server/__init__.py
+++ backend/notification_server/__init__.py
@@ -8,10 +8,25 @@ from celery import Celery
 from flask_socketio import SocketIO
 
 
+# flask
 app = Flask('Notification server')
-celery = Celery(
-    'Notification celery',
-    broker=os.environ.get('CELERY_BROKER_URL')
-)
+
+
+# socketio
 socket = SocketIO(app)
+
+
+# celery
+redis_host = os.environ.get('REDIS_HOST')
+redis_port = os.environ.get('REDIS_PORT')
+redis_queue_db = os.environ.get('REDIS_QUEUE_DB')
+
+redis_url = f'redis://{redis_host}:{redis_port}/{redis_queue_db}'
+celery = Celery(
+    'Feed celery',
+    broker=redis_url
+)
+
+
+# miscellaneous
 api_key = os.environ.get('API_KEY') or 'secret-api-key'
