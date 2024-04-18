09674488163cf33a48a13684b2069ea81db7b1a3
ChainsAre2Tight <124983704+ChainsAre2Tight@users.noreply.github.com>
2024-04-18 21:51:06 +0300

    some refactoring

--- backend/notification_server/api.py
+++ backend/notification_server/api.py
@@ -1,4 +1,4 @@
-
+"""Provides API reauests for notification server"""
 
 
 import requests
@@ -7,11 +7,21 @@ from notification_server import api_key
 
 
 def update_sid(session_id: str, sid: str) -> list[int]:
+    """Sends a request to notification server containing socket id and his parent session data
+
+    Args:
+        session_id (str): browser fingerprint
+        sid (str): socket id of incoming connection
+
+    Returns:
+        list[int]: list of chat ids that user has access to
+    """
     r = requests.patch(
         url='nginx://api/users/current/sessions/current',
         headers={'Authorization': api_key},
         data={'socket_id': sid, 'session_id': session_id},
         timeout=10
     )
-    assert r.status_code == 200, f'Request of chats failed with HTTP code {r.status_code}. Response: {r.json()}'
+    assert r.status_code == 200, f'Request of chats failed with HTTP code {r.status_code}.\
+ Response: {r.json()}'
     return [int(id_) for id_ in r.json()['data']['chats']]
--- backend/notification_server/controllers.py
+++ backend/notification_server/controllers.py
@@ -1,10 +1,12 @@
-
+"""Provides controllers"""
 
 
 from flask import request
 from flask_socketio import join_room, leave_room
+import jwt
 
 from libraries.utils.my_dataclasses import Token
+from libraries.utils.http_wrappers import decode_token
 
 from notification_server import socket
 from notification_server.api import update_sid
@@ -15,7 +17,12 @@ class EventController:
     @staticmethod
     @socket.on('connection')
     def handle_connection(data: dict, access_token: Token):
-        _ = data  # stub
+        try:
+            access_token = decode_token(raw_token=data['data'])
+        except (jwt.DecodeError, jwt.InvalidSignatureError, KeyError):
+            socket.emit('bad-token', to=request.sid)
+        except jwt.ExpiredSignatureError:
+            socket.emit('expired', to=request.sid)
         chats = update_sid(session_id=access_token.sessionId, sid=request.sid)
         RoomController.add_user_to_rooms(sid=request.sid, rooms=chats)
 
