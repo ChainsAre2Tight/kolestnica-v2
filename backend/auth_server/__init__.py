414ff460da857fe18afd7a77791fe50dbcef43b4
ChainsAre2Tight <124983704+ChainsAre2Tight@users.noreply.github.com>
2024-04-18 22:26:37 +0300

    adds api key

--- backend/auth_server/__init__.py
+++ backend/auth_server/__init__.py
@@ -7,8 +7,13 @@ from flask import Flask
 
 from libraries.database.models import db
 
-app = Flask('User server')
 
+# flask settings
+app = Flask('User server')
+app.config['SECRET_KEY'] = os.environ.get('FLASK-SECRET-KEY') or 'secret'
+
+
+# database settings
 db_url = os.environ.get('DB_URL')
 db_username = os.environ.get('DB_USERNAME')
 db_password = os.environ.get('DB_PASSWORD')
@@ -18,11 +23,14 @@ db_port = os.environ.get('DB_PORT')
 app.config['SQLALCHEMY_DATABASE_URI'] = \
     f'{db_url}://{db_username}:{db_password}@db:{db_port}/{db_name}'
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost:3306/koleso2_test'
-
-app.config['SECRET_KEY'] = os.environ.get('FLASK-SECRET-KEY') or 'secret'
 app.config['SQLALCHEMY_ECHO'] = False
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
+
+
+# miscellaneous
+api_key = os.environ.get('API_KEY') or 'secret-api-key'
+
 
 from auth_server.controllers import sessions_controller, tokens_controller, users_controller
