c50de2dfa632a3f18cf89e25052c363dbbe27aab
ChainsAre2Tight <124983704+ChainsAre2Tight@users.noreply.github.com>
2024-04-20 14:12:00 +0300

    fixes a typo

--- backend/feed_server/services/members/adder.py
+++ backend/feed_server/services/members/adder.py
@@ -15,9 +15,9 @@ class MemberAdder(MemberAdderInterface):
 
     @staticmethod
     @commit
-    def add_member(chat_id: int, target_id: int, broser_fingerprint: str) -> list[User]:
+    def add_member(chat_id: int, target_id: int, browser_fingerprint: str) -> list[User]:
 
-        user_id = quiry.get_user_id_by_browser_fingerprint(browser_fingerprint=broser_fingerprint)
+        user_id = quiry.get_user_id_by_browser_fingerprint(browser_fingerprint=browser_fingerprint)
         chat = quiry.get_chat_by_id(chat_id=chat_id)
         verify_user_in_chat(chat=chat, user_id=user_id)
 
